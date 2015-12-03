26d9b61983d0ceef7ded8300b99e5c591f5616b4
Meder <m.toktosunov@icloud.com>
2015-12-03 17:00:27 -0500

    Extra commit

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -1,16 +1,47 @@
 // YOUR CODE GOES HERE
-var clicks = 1;
+var page = 1;
 
-$("#more-sprouts").click(function(event) {
-  event.preventDefault();
-  clicks = clicks + 1;
-  var request = $.ajax({
-    method: "GET",
-    url: "/tweets.json?page=" + clicks
-  });
-  request.done(function(data) {
-    data.forEach(function(tweet) {
-      $(".tweets").append("<li class='tweet'> <div class='body'>" + tweet.text + "</div> <div class='user'>" + tweet.username + "</div> </li>");
+// $("#more-sprouts").click(function(event) {
+//   event.preventDefault();
+//   page = page + 1;
+//   var request = $.ajax({
+//     method: "GET",
+//     url: "/tweets.json?page=" + page
+//   });
+//   request.done(function(data) {
+//     data.forEach(function(tweet) {
+//       $(".tweets").append("<li class='tweet'> <div class='body'>" + tweet.text + "</div> <div class='user'>" + tweet.username + "</div> </li>");
+//     });
+//   });
+// });
+
+$(document).ready(function(){
+  function lastAddedLiveFunc(){
+    var request = $.ajax({
+      method: "GET",
+      url: "/tweets.json?page=" + page
     });
+    request.done(function(data){
+      data.forEach(function(tweet){
+        $(".tweets").append("<li class='tweet'> <div class='body'>" + tweet.text + "</div> <div class='user'>" + tweet.username + "</div> </li>");
+      });
+    });
+  };
+
+  if ($(document).height() == $(window).height()){
+    page = page + 1;
+    lastAddedLiveFunc();
+  }
+
+  $(window).scroll(function(){
+    var wintop = $(window).scrollTop(), docheight = $(document).height(), winheight = $(window).height();
+    var  scrolltrigger = .95;
+
+    if  ((wintop/(docheight-winheight)) > scrolltrigger){
+     //console.log('scroll bottom');
+       page = page + 1;
+       console.log(page);
+       lastAddedLiveFunc();
+    }
   });
 });
